21b9c560f6ced7c6529a4860abcdeae4c9d29ca0
berdemozdemir@gmail.com <berdemozdemir@gmail.com>
2025-04-21 20:33:29 +0300

    feat: add NotFoundPage component for handling 404 errors

---
+++ app/not-found.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import { Button } from '@/components/ui/Button';
+import { APP_NAME } from '@/lib/constants';
+import { paths } from '@/lib/constants/paths';
+import Image from 'next/image';
+import Link from 'next/link';
+
+const NotFoundPage = () => {
+  return (
+    <div className="flex h-screen w-full items-center justify-center">
+      <div className="flex flex-col items-center justify-center gap-4 rounded-2xl p-5 shadow-2xl">
+        <Image
+          src="/images/logo.svg"
+          alt={`${APP_NAME} logo`}
+          height={48}
+          width={48}
+          priority
+        />
+
+        <h1 className="text-3xl font-bold">404 - Page Not Found</h1>
+
+        <p className="text-lg">The page you are looking for does not exist.</p>
+
+        <Button asChild>
+          <Link href={paths.home}>Home Page</Link>
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default NotFoundPage;
