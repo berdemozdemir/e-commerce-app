e4405ba7e092421547b9b7b88a4120369bf8a69d
berdemozdemir@gmail.com <berdemozdemir@gmail.com>
2025-04-21 19:19:31 +0300

    feat: add header and footer components to root layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,6 @@
+import { Footer } from '@/components/Footer';
+import { Header } from '@/components/header';
+
 // the way of overriding the default metadata
 // it's gonna look like Home | My E-Commerce Store
 export const metadata = {
@@ -11,9 +14,13 @@ export default function RootLayout({
 }>) {
   return (
     <div className="flex h-screen flex-col">
+      <Header />
+
       <main className="w-full max-w-7xl flex-1 p-5 md:px-10 lg:mx-auto">
         {children}
       </main>
+
+      <Footer />
     </div>
   );
 }
