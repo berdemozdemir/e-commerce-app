d5641b375cc2888b65b7548600cbffbbd2b53601
berdemozdemir@gmail.com <berdemozdemir@gmail.com>
2025-04-21 20:27:43 +0300

    feat: create loading page component with spinner animation

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+export default function LoadingPage() {
+  return (
+    <div>
+      <div className="flex h-screen w-full items-center justify-center">
+        <div className="h-16 w-16 animate-spin rounded-full border-4 border-t-4 border-t-yellow-500"></div>
+      </div>
+    </div>
+  );
+}
