43ba79d32caf082233e8588e4e7421057ac8de7a
berdemozdemir@gmail.com <berdemozdemir@gmail.com>
2025-04-24 02:53:26 +0300

    feat: create product images for deatil page

---
+++ components/product/ProductImages.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import { cn } from '@/lib/utils';
+import Image from 'next/image';
+import { useState } from 'react';
+
+type ProductImagesProps = {
+  images: string[];
+};
+
+export const ProductImages = ({ images }: ProductImagesProps) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  return (
+    <div>
+      <Image
+        src={images[currentIndex]}
+        width={800}
+        height={800}
+        alt="product image"
+      />
+
+      <div className="flex gap-2">
+        {images.map((image, index) => (
+          <div
+            key={image}
+            className={cn(
+              'cursor-pointer border-2 hover:border-orange-300',
+              currentIndex === index && 'border-orange-300',
+            )}
+            onClick={() => setCurrentIndex(index)}
+          >
+            <Image
+              src={image}
+              alt="min product image"
+              width={100}
+              height={100}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
