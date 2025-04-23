fbb915b687674f133b080e7a798f15110dfbe82d
berdemozdemir@gmail.com <berdemozdemir@gmail.com>
2025-04-23 17:50:08 +0300

    feat: create product list component and show it on root

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,12 @@
-export default function Home() {
-  return <div>Hello world</div>;
+import { ProductList } from '@/components/product/ProductList';
+import { getLatestProducts } from '@/lib/actions/product.action';
+
+export default async function Home() {
+  const latestProducts = await getLatestProducts();
+
+  return (
+    <div>
+      <ProductList data={latestProducts} title="Newest Ones" limit={6} />
+    </div>
+  );
 }
---
+++ components/product/ProductList.tsx
@@ -0,0 +1,32 @@
+import { TProduct } from '@/lib/types/product';
+import { ProductCard } from './ProductCard';
+
+type ProductListProps = {
+  data: TProduct[];
+  title?: string;
+  limit?: number;
+};
+
+export const ProductList = ({ data, title, limit }: ProductListProps) => {
+  const limitedProducts = limit ? data.slice(0, limit) : data;
+
+  return (
+    <div className="my-10">
+      <h1 className="mb-4 text-center text-2xl font-bold sm:text-start">
+        {title}
+      </h1>
+
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        {limitedProducts.map((product: TProduct) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+
+        {limitedProducts.length === 0 && (
+          <div className="col-span-4 text-center">
+            <p className="text-gray-500">No products found.</p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
