bf246959640cceffd416e0d555b364340be214b9
berdemozdemir@gmail.com <berdemozdemir@gmail.com>
2025-04-23 17:51:16 +0300

    fix: redesigned button variant in not found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -22,7 +22,7 @@ const NotFoundPage = () => {
 
         <p className="text-lg">The page you are looking for does not exist.</p>
 
-        <Button asChild>
+        <Button asChild variant="ghost" className="border">
           <Link href={paths.home}>Home Page</Link>
         </Button>
       </div>
