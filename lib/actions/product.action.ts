9688f13a196bc15d04189b7929c9f69b1083aa4a
berdemozdemir@gmail.com <berdemozdemir@gmail.com>
2025-04-23 18:03:48 +0300

    feat: create server function for fetching product by slug

--- lib/actions/product.action.ts
+++ lib/actions/product.action.ts
@@ -2,13 +2,21 @@
 
 import { products } from '@/server';
 import { db } from '@/server/drizzle-client';
-import { desc } from 'drizzle-orm';
+import { desc, eq } from 'drizzle-orm';
 import { LATEST_PRODUCTS_LIMIT } from '../constants/product';
 
 export async function getLatestProducts() {
   const data = await db.query.products.findMany({
     limit: LATEST_PRODUCTS_LIMIT,
     orderBy: desc(products.createdAt),
+  });
+
+  return data;
+}
+
+export async function getProductBySlug(slug: string) {
+  const data = await db.query.products.findFirst({
+    where: eq(products.slug, slug),
   });
 
   return data;
