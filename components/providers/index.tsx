82bc1d2c1c315f3df9fb52d1a63e9703c7a0f540
berdemozdemir@gmail.com <berdemozdemir@gmail.com>
2025-05-03 23:15:28 +0300

    feat: add toast container and query client provider to provider component

--- components/providers/index.tsx
+++ components/providers/index.tsx
@@ -1,7 +1,26 @@
 'use client';
 
+import queryClient from '@/lib/queryClient';
+import { QueryClientProvider } from '@tanstack/react-query';
 import { SessionProvider } from 'next-auth/react';
+import { FC, PropsWithChildren } from 'react';
+import { ToastContainer } from 'react-toastify';
 
-export const Providers = ({ children }: { children: React.ReactNode }) => {
-  return <SessionProvider>{children}</SessionProvider>;
-};
+export const Providers: FC<PropsWithChildren> = ({ children }) => (
+  <QueryClientProvider client={queryClient}>
+    <SessionProvider>
+      {children}
+
+      <ToastContainer
+        position="bottom-right"
+        autoClose={3000}
+        newestOnTop
+        hideProgressBar
+        closeOnClick
+        pauseOnHover
+        draggable={false}
+        theme="light"
+      />
+    </SessionProvider>
+  </QueryClientProvider>
+);
