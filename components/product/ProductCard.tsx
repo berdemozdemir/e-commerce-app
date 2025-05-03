347e53630110ac126f158cd14df3ef0082a1dadf
berdemozdemir@gmail.com <berdemozdemir@gmail.com>
2025-05-03 21:34:52 +0300

    refactor: refactor ProductCard to use Link component for product image and details

--- components/product/ProductCard.tsx
+++ components/product/ProductCard.tsx
@@ -9,30 +9,33 @@ export type ProductCardProps = {
 
 export const ProductCard = ({ product }: ProductCardProps) => {
   return (
-    <div className="mx-auto w-full max-w-sm space-y-4 rounded-md border shadow-md">
+    <Link
+      href={paths.productDetail(product.slug)}
+      className="mx-auto h-full w-full max-w-sm space-y-4 overflow-hidden rounded-md border shadow-md"
+    >
       <div>
-        <Link href={paths.productDetail(product.slug)} className="block">
-          <Image
-            src={product.images[0]}
-            alt={product.name}
-            width={300}
-            height={300}
-          />
-        </Link>
+        <Image
+          src={product.images[0]}
+          alt={product.name}
+          width={300}
+          height={300}
+        />
       </div>
 
-      <div className="p-4">
-        <h2 className="line-clamp-2 text-lg font-semibold">{product.name}</h2>
+      <div className="flex h-full max-h-40 flex-col justify-between p-4">
+        <div>
+          <h2 className="line-clamp-2 text-lg font-semibold">{product.name}</h2>
 
-        <p className="line-clamp-2 text-sm text-gray-500">
-          {product.description}
-        </p>
+          <p className="line-clamp-2 text-sm text-gray-500">
+            {product.description}
+          </p>
+        </div>
 
         <div className="mt-2 flex items-center justify-between">
           <span className="text-sm text-gray-500">{product.rating}</span>
           <span className="text-lg font-bold">${product.price}</span>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
