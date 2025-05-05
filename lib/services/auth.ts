f62bc441b6dbe0b6491074d7507c31b88e06523c
berdemozdemir@gmail.com <berdemozdemir@gmail.com>
2025-05-05 03:14:10 +0300

    refactor: simplify mutation functions and improve error handling in authentication

--- lib/services/auth.ts
+++ lib/services/auth.ts
@@ -1,33 +1,32 @@
 import { useMutation } from '@tanstack/react-query';
 import { signInWithCredentials, signUpUser } from '../actions/user.action';
 import { toast } from 'react-toastify';
+import { TSignupFormSchemaRequest } from '../schemas/auth/sign-up.schema';
 
 export const useLoginMutation = () =>
   useMutation({
     mutationFn: (formData: FormData) => {
-      return signInWithCredentials(null, formData).then((res) => {
+      return signInWithCredentials(formData).then((res) => {
         if (res.error) throw res.error;
         return res.data;
       });
     },
     onError: (error) => {
       console.error('Login error:', error);
-      toast.error('Login failed. Please try again.');
       toast.error(error.message);
     },
   });
 
 export const useSignupMutation = () =>
   useMutation({
-    mutationFn: (formData: FormData) => {
-      return signUpUser(null, formData).then((res) => {
+    mutationFn: (data: TSignupFormSchemaRequest) => {
+      return signUpUser(data).then((res) => {
         if (res.error) throw res.error;
         return res.data;
       });
     },
     onError: (error) => {
       console.error('Signup error:', error);
-      const msg = error instanceof Error ? error.message : 'Signup failed.';
-      toast.error(msg);
+      toast.error(error.message);
     },
   });
