3a02d832d9f4bbc05db824ff31f639d471691417
berdemozdemir@gmail.com <berdemozdemir@gmail.com>
2025-05-05 03:15:01 +0300

    refactor: enhance password validation and error handling in signUpFormSchema

--- lib/schemas/auth/sign-up.schema.ts
+++ lib/schemas/auth/sign-up.schema.ts
@@ -4,11 +4,45 @@ export const signUpFormSchema = z
   .object({
     name: z.string().min(3, 'Name must be at least 3 characters long'),
     email: z.string().email('Invalid email address'),
-    password: z.string().min(6, 'Password must be at least 6 characters long'),
-    confirmPassword: z
-      .string()
-      .min(6, 'Password must be at least 6 characters long'),
+    password: z.string({
+      required_error: 'password is required',
+    }),
+    confirmPassword: z.string({
+      required_error: 'password is required',
+    }),
   })
-  .refine((data) => data.password === data.confirmPassword, {
-    message: 'Passwords do not match',
+  .superRefine((data, ctx) => {
+    if (data.password !== data.confirmPassword) {
+      ctx.addIssue({
+        path: ['confirmPassword'],
+        code: z.ZodIssueCode.custom,
+        message: 'Passwords do not match',
+      });
+    }
+
+    if (
+      !/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z0-9])/.test(data.password)
+    ) {
+      ctx.addIssue({
+        path: ['password'],
+        code: z.ZodIssueCode.custom,
+        message:
+          'password must contain at least one uppercase letter, one lowercase letter, one number, and one special character',
+      });
+    }
+
+    if (
+      !/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z0-9])/.test(
+        data.confirmPassword,
+      )
+    ) {
+      ctx.addIssue({
+        path: ['password'],
+        code: z.ZodIssueCode.custom,
+        message:
+          'password must contain at least one uppercase letter, one lowercase letter, one number, and one special character',
+      });
+    }
   });
+
+export type TSignupFormSchemaRequest = z.infer<typeof signUpFormSchema>;
