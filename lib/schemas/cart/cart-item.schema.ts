1ab66592e5744f506273329d15094b35428d4f5d
berdemozdemir@gmail.com <berdemozdemir@gmail.com>
2025-12-04 17:08:55 +0300

    fix: update price validation regex to allow up to 2 decimal places in cart schemas

--- lib/schemas/cart/cart-item.schema.ts
+++ lib/schemas/cart/cart-item.schema.ts
@@ -7,44 +7,40 @@ export const cartItemSchema = z.object({
   slug: z.string().min(1, { message: 'Slug is required' }),
   quantity: z.number().positive(),
   image: z.string().min(1, { message: 'Image is required' }),
-  price: z
-    .string()
-    .refine(
-      (val) => /^\d+(\d{2})?$/.test(convertNumberToDecimal(Number(val))),
-      { message: 'Price must be a valid number with up to 2 decimal places' },
-    ),
+  price: z.string(),
 });
 
 // TODO: move these types to type folder
 export type TCartItem = z.infer<typeof cartItemSchema>;
 
 export const cartSchema = z.object({
-  userId: z.string().min(1, 'Session cart id is reqiured'),
-  sessionCardId: z.string().optional().nullable(),
+  id: z.string(),
+  userId: z.string().min(1, 'Session cart id is reqiured').optional(),
+  sessionCartId: z.string(),
   items: z.array(cartItemSchema),
   //   TODO: make these refines a util function
   itemsPrice: z
     .string()
     .refine(
-      (val) => /^\d+(\d{2})?$/.test(convertNumberToDecimal(Number(val))),
+      (val) => /^\d+(\.\d{1,2})?$/.test(convertNumberToDecimal(Number(val))),
       { message: 'Price must be a valid number with up to 2 decimal places' },
     ),
   totalPrice: z
     .string()
     .refine(
-      (val) => /^\d+(\d{2})?$/.test(convertNumberToDecimal(Number(val))),
+      (val) => /^\d+(\.\d{1,2})?$/.test(convertNumberToDecimal(Number(val))),
       { message: 'Price must be a valid number with up to 2 decimal places' },
     ),
   shippingPrice: z
     .string()
     .refine(
-      (val) => /^\d+(\d{2})?$/.test(convertNumberToDecimal(Number(val))),
+      (val) => /^\d+(\.\d{1,2})?$/.test(convertNumberToDecimal(Number(val))),
       { message: 'Price must be a valid number with up to 2 decimal places' },
     ),
   taxPrice: z
     .string()
     .refine(
-      (val) => /^\d+(\d{2})?$/.test(convertNumberToDecimal(Number(val))),
+      (val) => /^\d+(\.\d{1,2})?$/.test(convertNumberToDecimal(Number(val))),
       { message: 'Price must be a valid number with up to 2 decimal places' },
     ),
 });
