2a70a62c6bbd0e136c11bef4ec5c228701b09ce5
berdemozdemir@gmail.com <berdemozdemir@gmail.com>
2025-12-04 17:08:08 +0300

    feat: add utility functions for price calculation and rounding

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,6 @@
 import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
+import { TCartItem } from './schemas/cart/cart-item.schema';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -11,10 +12,40 @@ export const convertNumberToDecimal = (num: number): string => {
   return decimal ? `${int}.${decimal}` : `${int}.00`;
 };
 
+export const convertToPlainObject = <T>(value: T): T =>
+  JSON.parse(JSON.stringify(value));
+
 export const getTwoLetterInitials = (name: string): string => {
   const names = name.split(' ');
 
   if (names.length < 2) return names[0].charAt(0).toUpperCase();
 
   return `${names[0].charAt(0).toUpperCase()}${names[1].charAt(0).toUpperCase()}`;
+};
+
+export const roundTwo = (value: string | number) => {
+  if (typeof value === 'string') {
+    return Math.round(Number(value) * 100) / 100;
+  } else if (typeof value === 'number') {
+    return Math.round(value * 100) / 100;
+  } else {
+    throw new Error('Value must be a string or number');
+  }
+};
+
+export const calculatePrice = (items: TCartItem[]) => {
+  const itemsPrice = roundTwo(
+    items.reduce((acc, item) => acc + Number(item.price) * item.quantity, 0),
+  );
+
+  const taxPrice = roundTwo(itemsPrice * 0.15);
+  const shippingPrice = roundTwo(itemsPrice > 100 ? 0 : 10);
+  const totalPrice = roundTwo(itemsPrice + shippingPrice + taxPrice);
+
+  return {
+    taxPrice: taxPrice.toFixed(2),
+    shippingPrice: shippingPrice.toFixed(2),
+    itemsPrice: itemsPrice.toFixed(2),
+    totalPrice: totalPrice.toFixed(2),
+  };
 };
