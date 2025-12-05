fb00683e10c6ab5a4790144b7c0ee6f48ac0a154
berdemozdemir@gmail.com <berdemozdemir@gmail.com>
2025-12-06 02:21:34 +0300

    fix: add go to shopping button to improve ux in empty cart component

--- components/cart/EmptyCart.tsx
+++ components/cart/EmptyCart.tsx
@@ -1,14 +1,20 @@
 import { FC } from 'react';
 import { Button } from '../ui/Button';
+import { useRouter } from 'next/navigation';
+import { paths } from '@/lib/constants/paths';
 
-export const EmptyCart: FC = () => (
-  <div className="p-6 text-center text-gray-600">
-    <p className="mb-4">
-      Your cart is empty. You have not added any products yet. Browse our
-      collection and add items to your cart to see them here. Once you add
-      products, they will appear in your cart and you can proceed to checkout.
-    </p>
+export const EmptyCart: FC = () => {
+  const router = useRouter();
 
-    <Button>Shop Now</Button>
-  </div>
-);
+  return (
+    <div className="p-6 text-center text-gray-600">
+      <p className="mb-4">
+        Your cart is empty. You have not added any products yet. Browse our
+        collection and add items to your cart to see them here. Once you add
+        products, they will appear in your cart and you can proceed to checkout.
+      </p>
+
+      <Button onClick={() => router.push(paths.home)}>Shop Now</Button>
+    </div>
+  );
+};
