730eae0bae4960734a4d6e6b6793a7161580cd4c
berdemozdemir@gmail.com <berdemozdemir@gmail.com>
2025-12-10 03:30:10 +0300

    feat: add useUpdateUserPaymentMethodMutation for updating user payment methods

--- lib/services/user.ts
+++ lib/services/user.ts
@@ -2,9 +2,16 @@ import { useMutation } from '@tanstack/react-query';
 import { updateUserAddress } from '../actions/user/update-user-address';
 import { TShippingAddressSchema } from '../schemas/shipping-address';
 import { okOrThrow } from '../result';
+import { updatePaymentMethods } from '../actions/user/update-payment-methods';
 
 export const useUpdateUserAddressMutation = () =>
   useMutation({
     mutationFn: (address: TShippingAddressSchema) =>
       updateUserAddress(address).then(okOrThrow),
+  });
+
+export const useUpdateUserPaymentMethodMutation = () =>
+  useMutation({
+    mutationFn: (paymentMethod: { paymentMethod: string }) =>
+      updatePaymentMethods(paymentMethod).then(okOrThrow),
   });
