6b818b527aec6042dfad0a4d4e7e4e5ba97a8b56
berdemozdemir@gmail.com <berdemozdemir@gmail.com>
2026-01-25 17:44:18 +0300

    feat: implement getOrdersByAdmin function for admin order retrieval

---
+++ lib/actions/admin/get-orders-by-admin.ts
@@ -0,0 +1,43 @@
+import { auth } from '@/lib/auth';
+import { failure, isFailure, ok, Result, tryCatch } from '@/lib/result';
+import { TMyOrders } from '@/lib/types/myOrders';
+import { orders } from '@/server';
+import { db } from '@/server/drizzle-client';
+
+// TODO: add a pagination or infinite scroll to this data
+
+export const getOrdersByAdmin = async (): Promise<Result<TMyOrders[]>> => {
+  const session = await auth();
+  const userId = session?.user?.id;
+
+  if (!userId) return failure('Unauthorized');
+  if (session?.user.role !== 'admin') return failure('Forbidden');
+
+  const response = await tryCatch(
+    db
+      .select({
+        id: orders.id,
+        createdAt: orders.createdAt,
+        isPaid: orders.isPaid,
+        paidAt: orders.paidAt,
+        isDelivered: orders.isDelivered,
+        deliveredAt: orders.deliveredAt,
+        totalPrice: orders.totalPrice,
+      })
+      .from(orders),
+  );
+
+  if (isFailure(response)) return failure(response.error);
+
+  const myOrders: TMyOrders[] = response.data.map((item) => ({
+    id: item.id,
+    createdAt: item.createdAt.toString(),
+    totalPrice: item.totalPrice,
+    isPaid: item.isPaid,
+    paidAt: item.paidAt?.toString() ?? undefined,
+    isDelivered: item.isDelivered,
+    deliveredAt: item.deliveredAt?.toString() ?? undefined,
+  }));
+
+  return ok(myOrders);
+};
