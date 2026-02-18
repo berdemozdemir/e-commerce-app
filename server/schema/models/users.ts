955b623330b1c4dfb17f38350583b1f3cba7430f
berdemozdemir@gmail.com <berdemozdemir@gmail.com>
2026-02-18 05:52:26 +0300

    fix: set default role to User in users table schema

--- server/schema/models/users.ts
+++ server/schema/models/users.ts
@@ -1,6 +1,6 @@
 import { TAddress } from '@/lib/types/address';
 import { getPaymentMethods } from '@/lib/types/payment-methods';
-import { getRoles } from '@/lib/types/role';
+import { getRoles, Role } from '@/lib/types/role';
 import {
   boolean,
   timestamp,
@@ -30,8 +30,7 @@ export const users = pgTable('user', {
   // TODO: take a look at this, it should be a string or date ??
   // can we store a date in a db column?
   emailVerified: timestamp('email_verified', { mode: 'date' }),
-  image: text('image'),
-  role: roleEnum('role').notNull(),
+  role: roleEnum('role').default(Role.User).notNull(),
   // TODO: make this address array type, so we can store multiple addresses and user can choose any of them
   // TODO: make this address column normalized in a separate table
   address: jsonb('address').$type<TAddress | null>(),
