37154c40fe49c3c5929a0627dd9583afa5a91003
berdemozdemir@gmail.com <berdemozdemir@gmail.com>
2026-02-23 01:46:18 +0300

    feat: add AdminSearch component for improved admin search functionality

---
+++ components/header/AdminSearch.tsx
@@ -0,0 +1,37 @@
+import { debounce } from '@/lib/utils/debounce';
+import { useEffect, useState } from 'react';
+import { Input } from '../ui/Input';
+import { useCallback } from 'react';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+
+export const AdminSearch = () => {
+  const [searchValue, setSearchValue] = useState('');
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const updateSearchQuery = useCallback(
+    debounce((value: string) => {
+      const params = new URLSearchParams(searchParams.toString());
+      value ? params.set('query', value) : params.delete('query');
+      router.push(params ? `${pathname}?${params}` : pathname);
+    }, 500),
+    [router, pathname, searchParams],
+  );
+
+  useEffect(() => {
+    setSearchValue(searchParams.get('query') ?? '');
+  }, [searchParams]);
+
+  return (
+    <Input
+      placeholder="Search"
+      className="w-full max-w-xs"
+      value={searchValue}
+      onChange={(e) => {
+        setSearchValue(e.target.value);
+        updateSearchQuery(e.target.value);
+      }}
+    />
+  );
+};
