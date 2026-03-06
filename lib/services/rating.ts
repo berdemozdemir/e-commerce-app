c4768a6fbba9a1c8c415f521ed5d557724336218
berdemozdemir@gmail.com <berdemozdemir@gmail.com>
2026-03-06 04:29:44 +0300

    feat: add useGetProductRatingsQuery hook and rating types for improved product rating functionality

--- lib/services/rating.ts
+++ lib/services/rating.ts
@@ -1,10 +1,17 @@
-import { useMutation } from '@tanstack/react-query';
-import { TCreateRatingSchema } from '../types/create-rating-schema';
+import { useMutation, useQuery } from '@tanstack/react-query';
+import { TCreateRatingSchema } from '../types/rating';
 import { createRating } from '../actions/rating/create-rating';
 import { okOrThrow } from '../result';
+import { getProductRatings } from '../actions/rating/get-product-ratings';
 
 export const useCreateRatingMutation = () =>
   useMutation({
     mutationFn: (payload: TCreateRatingSchema) =>
       createRating(payload).then(okOrThrow),
+  });
+
+export const useGetProductRatingsQuery = (productId: string) =>
+  useQuery({
+    queryKey: ['product-ratings', productId],
+    queryFn: () => getProductRatings({ productId }).then(okOrThrow),
   });
--- lib/types/rating.ts
+++ lib/types/rating.ts
@@ -1,4 +1,14 @@
 import { createRatingSchema } from '@/lib/schemas/rating/create-rating';
 import { z } from 'zod';
 
+export type Rating = {
+  id: string;
+  productId: string;
+  userName: string;
+  title: string;
+  rating: number;
+  comment?: string;
+  createdAt: string;
+};
+
 export type TCreateRatingSchema = z.infer<typeof createRatingSchema>;
