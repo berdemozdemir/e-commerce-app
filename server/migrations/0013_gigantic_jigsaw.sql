5b7141193c1891be2889b3ff3d96fc7c8aa7fbdc
berdemozdemir@gmail.com <berdemozdemir@gmail.com>
2026-03-06 04:00:13 +0300

    chore: add migrations

---
+++ server/migrations/0013_gigantic_jigsaw.sql
@@ -0,0 +1,13 @@
+CREATE TABLE "ratings" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"product_id" uuid NOT NULL,
+	"user_id" uuid NOT NULL,
+	"title" text NOT NULL,
+	"rating" integer NOT NULL,
+	"comment" text,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+ALTER TABLE "ratings" ADD CONSTRAINT "ratings_product_id_products_id_fk" FOREIGN KEY ("product_id") REFERENCES "public"."products"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "ratings" ADD CONSTRAINT "ratings_user_id_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."user"("id") ON DELETE cascade ON UPDATE no action;
