8ea2fcd2bdcacd0084444a4d8c7aec730f41d659
berdemozdemir@gmail.com <berdemozdemir@gmail.com>
2026-03-06 04:02:33 +0300

    feat: add createRating function to handle user ratings and feedback submission

---
+++ lib/actions/rating/create-rating.ts
@@ -0,0 +1,39 @@
+'use server';
+
+import { auth } from '@/lib/auth';
+import { failure, isFailure, ok, Result, tryCatch } from '@/lib/result';
+import { TCreateRatingSchema } from '@/lib/types/create-rating-schema';
+import { createRatingSchema } from '@/lib/schemas/rating/create-rating';
+import { db } from '@/server/drizzle-client';
+import { ratings } from '@/server';
+
+export const createRating = async (
+  payload: TCreateRatingSchema,
+): Promise<Result<void>> => {
+  const session = await auth();
+  if (!session?.user)
+    return failure('User account not found. Please log in again.s');
+
+  const userId = session.user.id;
+  if (!userId) return failure('User account not found. Please log in again.');
+
+  const parsedPayload = createRatingSchema.safeParse(payload);
+
+  if (!parsedPayload.success)
+    return failure(parsedPayload.error.issues[0]?.message ?? 'Invalid payload');
+
+  const response = await tryCatch(
+    db.insert(ratings).values({
+      productId: parsedPayload.data.productId,
+      title: parsedPayload.data.title,
+      rating: parsedPayload.data.rating,
+      comment: parsedPayload.data.comment,
+      userId,
+    }),
+  );
+
+  if (isFailure(response))
+    return failure(response.error ?? 'Failed to create rating');
+
+  return ok(undefined);
+};
