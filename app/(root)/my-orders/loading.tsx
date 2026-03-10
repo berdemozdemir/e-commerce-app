f09adc57bb3e8b2fdb185a73457bf1950c70f0a7
berdemozdemir@gmail.com <berdemozdemir@gmail.com>
2026-03-11 02:20:28 +0300

    feat: add MyOrdersLoading component to display loading skeleton for my orders page

---
+++ app/(root)/my-orders/loading.tsx
@@ -0,0 +1,67 @@
+import { Skeleton } from '@/components/ui/Skeleton';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/Table';
+
+const MyOrdersLoading = () => (
+  <div>
+    <Skeleton className="mb-4 h-8 w-56" />
+    <div className="rounded-md border p-3">
+      <Table className="mb-4">
+        <TableHeader>
+          <TableRow>
+            <TableHead>
+              <Skeleton className="h-4 w-12" />
+            </TableHead>
+            <TableHead>
+              <Skeleton className="h-4 w-12" />
+            </TableHead>
+            <TableHead>
+              <Skeleton className="h-4 w-12" />
+            </TableHead>
+            <TableHead>
+              <Skeleton className="h-4 w-12" />
+            </TableHead>
+            <TableHead>
+              <Skeleton className="h-4 w-16" />
+            </TableHead>
+            <TableHead>
+              <Skeleton className="h-4 w-14" />
+            </TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {Array.from({ length: 4 }).map((_, i) => (
+            <TableRow key={i}>
+              <TableCell>
+                <Skeleton className="h-4 w-14" />
+              </TableCell>
+              <TableCell>
+                <Skeleton className="h-4 w-20" />
+              </TableCell>
+              <TableCell>
+                <Skeleton className="h-4 w-12" />
+              </TableCell>
+              <TableCell>
+                <Skeleton className="h-4 w-16" />
+              </TableCell>
+              <TableCell>
+                <Skeleton className="h-4 w-20" />
+              </TableCell>
+              <TableCell>
+                <Skeleton className="h-4 w-20" />
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  </div>
+);
+
+export default MyOrdersLoading;
--- app/(root)/my-orders/page.tsx
+++ app/(root)/my-orders/page.tsx
@@ -3,7 +3,6 @@ import { MyOrdersPage } from '@/components/my-orders/MyOrdersPage';
 import { getMyOrders } from '@/lib/actions/get-my-orders';
 import { paths } from '@/lib/constants/paths';
 
-// TOOD: create a skeleton for this page
 export default async function MyOrders() {
   const [err, items] = await getMyOrders();
 
